3b60d612f8e37ed0a1fced244158f904af7c1a2d
Stephanie Adams <adamsstephaniem@gmail.com>
2016-06-15 18:35:53 -0400

    Page renders with tweet data and styling

--- app.js
+++ app.js
@@ -11,25 +11,32 @@ app.listen(3000, function () {
 	console.log("Listening");
 
 })
+
+var routes = require('./routes/');
+app.use('/', routes);
+
 app.use("/:path",function (req,res,next){
 	console.log(req.method + " /" + req.params.path);
-	
 	next();
-
 });
+
 app.use('/special', function(req,res,next) {
 	console.log("User is special!");
 	console.log(res.statusCode);
 	next();
 });
+app.use(express.static('public'));
 
 
-app.get('/', function(req,res,next){
-	var people = [{name: "Zaphod"}, {name: "Arthur"}, {name: "Trillian"}]
-	res.render("index", {title: "Home Sweet Home", people: people})
-});
 
-app.get('/special', function(req,res,next){
-	res.send('Hi!\n');
 
-});
+
+// app.get('/', function(req,res,next){
+// 	var people = [{name: "Zaphod"}, {name: "Arthur"}, {name: "Trillian"}]
+// 	res.render("index", {title: "Home Sweet Home", people: people})
+// });
+
+// app.get('/special', function(req,res,next){
+// 	res.send('Hi!\n');
+
+// });
