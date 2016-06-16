6d70dc20e1b33f5119ddbcc0fbd52bdb403706ad
Stephanie Adams <adamsstephaniem@gmail.com>
2016-06-15 20:34:22 -0400

    Add form (not yet functional)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var swig = require('swig')
 
+
 app.engine("html", swig.renderFile);
 app.set("view engine", "html");
 app.set("views", __dirname + "/views");
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
 });
 
 router.get('/users/:name', function(req, res) {
@@ -21,5 +24,17 @@ router.get('/tweets/:id', function(req, res) {
 	console.log(list);
 	res.render('index', {title: "This Awesome Tweet", tweets: list} )
 	});
+
+router.post('/',urlencodedParser,function (req, res,next) {
+	if(!req.body){res.sendStatus(400)}	
+	res.render( 'index', { title: 'Twitter.js', tweets: [], showForm: true } ); //POST does not work "cannot POST /"
+	console.log(req.body.name);
+	next();
+});
+
+router.post('/',function (req, res,next) {
+	
+	next();
+})
 
 module.exports = router;
